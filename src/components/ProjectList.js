193b1820a506ab47df5048b5a4d20c62003e0389
alexander.Khatamov <alexk2120@gmail.com>
2021-11-30 06:09:23 -0500

    commit tree d0171809f99c9725fee065c36e5a2564def5ea2c

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,10 +3,26 @@ import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
   console.log(projects);
+  
+  // projects.filter(technologies)
+ const projOneName = projects[0].name 
+const projOneAbout = projects[0].about  
+
+const projTwoName = projects[1].name 
+const projTwoAbout= projects[1].about  
+
+const projThreeName = projects[2].name
+const projThreeAbout= projects[2].about  
+
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        <ProjectItem key = "1" name = {projOneName} about ={projOneAbout} />
+        <ProjectItem key = "2" name = {projTwoName} about ={projTwoAbout} />
+        <ProjectItem key = "3" name = {projThreeName} about ={projThreeAbout} /> 
+      
+      </div>
     </div>
   );
 }
